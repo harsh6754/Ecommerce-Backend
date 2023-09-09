eb52d07dbd94d6a7e151fa0cbd8898a9f32ab348
harsh6754 <210306105012@paruluniversity.ac.in>
2023-09-09 22:47:56 +0530

    Ecommerce Backend

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -23,12 +23,82 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     taxPrice,
     shippingPrice,
     totalPrice,
-    paidAt:Date.now(),
-    user:req.user._id,
-
+    paidAt: Date.now(),
+    user: req.user._id,
   });
   res.status(201).json({
-    success:true,
+    success: true,
     order,
-  })
+  });
 });
+
+//get Single Order
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (!order) {
+    return next(new ErrorHander("Order Not Found ", 404));
+  }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+//Get User Order Details Show With User
+
+exports.myOrder = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+//get All Orders
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+  orders.forEach((order)=>{
+    totalAmount+=order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+//update Order Status Order
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.find(req.params.id);
+
+  if(order.orderStatus ==="Delivered"){
+    return next(new ErrorHander("You have already delivered this order",400));
+  }
+
+   order.orderItems.forEach(async order=>{
+    await updateStock(order.product,order.quantity);
+   });
+
+   order.orderStatus = req.body.status;
+   if(req.body.status==="Delivered"){
+    order.deliveredAt = Date.now();
+   }
+  await order.save({ validateBeforeSave:false });
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id,quantity){
+   const product = await Product.findById(id);
+
+   product.Stock -=quantity;
+   await product.save({validateBeforeSave:false})
+}
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const{isAuthenticatedUser,authorizeRoles} = require("../middleware/auth");
-const { newOrder } = require("../controllers/orderController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrder,
+} = require("../controllers/orderController");
 
-router.route("/order/new").post(isAuthenticatedUser,newOrder);
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
+router.route("/orders/me").get(isAuthenticatedUser, myOrder);
 
 module.exports = router;
